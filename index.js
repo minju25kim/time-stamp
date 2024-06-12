4dfac7fc7ab1254ac74ebd0c54fc75e00bf57353
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2024-06-12 17:18:36 +0900

    second commit

--- index.js
+++ index.js
@@ -2,31 +2,46 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
-
+app.get("/api/:input", function (req, res) {
+  const input = req.params.input;
+  let dateObject;
+  if (isNaN(input)) {
+    dateObject = new Date(input);
+  } else {
+    const unixTimeStamp = parseInt(input);
+    dateObject = new Date(unixTimeStamp);
+  }
+  const unixDate = dateObject.getTime();
+  const utcDate = dateObject.toUTCString();
+  res.json({ unix: unixDate, utc: utcDate });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + 'http://localhost:'+listener.address().port);
+  console.log(
+    "Your app is listening on port " +
+      "http://localhost:" +
+      listener.address().port
+  );
 });
